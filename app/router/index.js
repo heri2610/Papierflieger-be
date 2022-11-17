13bf9c2116a7b417e4ee5366f97797d24f069167
heri2610 <suhaeriheri45@gmail.com>
2022-11-17 22:17:52 +0700

    menambahkan root response

--- app/router/index.js
+++ app/router/index.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('../../docs/swagger.json');
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("../../docs/swagger.json");
 const Auth = require("./auth");
 
-router.use('/api-docs', swaggerUI.serve);
-router.get('/api-docs', swaggerUI.setup(swaggerDocument));
+router.use("/api-docs", swaggerUI.serve);
+router.get("/api-docs", swaggerUI.setup(swaggerDocument));
+
+router.get("/api-docs", (req, res) => {
+  res.status(200).json({
+    message: "API is running",
+  });
+});
 
 router.use("/api/auth", Auth);
 
