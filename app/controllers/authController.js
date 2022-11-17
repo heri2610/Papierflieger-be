defaac228474b5f75001e3932050f8a12894af80
heri2610 <suhaeriheri45@gmail.com>
2022-11-17 16:57:00 +0700

    controller

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -35,18 +35,15 @@ const register = async (req, res) => {
     const User = await users.findOne({ where: { email } });
 
     // validasi
-    // if (!email) throw new ApiError(400, "email cannot be empty");
-    // if (!fullname) throw new ApiError(400, "name cannot be empty");
-    // if (!password) throw new ApiError(400, "password cannot be empty");
-    // if (User) throw new ApiError(400, "email already exist!");
+    if (!fullname || !email || !password || !gelar || !kebangsaan || !negara || !username || !hp || !provinsi || !tanggal_lahir || !kabupaten) throw new ApiError(400, "filed cannot be empty");
+    if (User) throw new ApiError(400, "email already exist!");
     if (password.length < 8) throw new ApiError(400, "minimum password length must be 8 charater or more");
 
     // hash password
     const hashedPassword = bcrypt.hashSync(password, 10);
-
     const newUser = await users.create({ fullname, email, hashedPassword, gelar, kebangsaan, negara, username, hp, provinsi, tanggal_lahir, kabupaten });
     res.status(200).json({
-      message: "registrasi success",
+      message: "registrasi berhasil, silahkan login",
       newUser,
     });
   } catch (error) {
