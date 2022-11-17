457facfc41bb660e71384bedfb9338f5656c4597
heri2610 <suhaeriheri45@gmail.com>
2022-11-17 23:24:04 +0700

    konfigurasi db production

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER } = process.env;
 const db = {
   development: {
     username: DB_USER,
@@ -15,10 +16,11 @@ const db = {
     dialect: "mysql",
   },
   production: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
-    host: DB_HOST,
+    username: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASE,
+    port: MYSQLPORT,
+    host: MYSQLHOST,
     dialect: "mysql",
   },
 };
