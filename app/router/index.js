67c969a6b67ba4395359bd853de8c81bbd14702f
heri2610 <suhaeriheri45@gmail.com>
2022-11-17 22:47:12 +0700

    root halaman fix

--- app/router/index.js
+++ app/router/index.js
@@ -7,7 +7,7 @@ const Auth = require("./auth");
 router.use("/api-docs", swaggerUI.serve);
 router.get("/api-docs", swaggerUI.setup(swaggerDocument));
 
-router.get("/api-docs", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({
     message: "API is running",
   });
