c832d4a22149ca1c2a76d1d0cb1b71b3646e7b50
heri2610 <suhaeriheri45@gmail.com>
2022-11-23 09:55:44 +0700

    konfigurasi imageKit dan muelter

--- app/router/auth.js
+++ app/router/auth.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 const { login, register, verified } = require('../controllers/authController');
+// gunakan ini jika ada upload mage
+// const Uploader = require('../middlewares/uploader');
+// ini contoh
+// router.post(
+//   '/login',
+//   Authentication,
+//   Uploader.single('image'),
+//   login
+// );
 
 router.post('/login', login);
 router.post('/register', register);
---
+++ midleware/uploader.js
@@ -0,0 +1,4 @@
+const multer = require('multer');
+const upload = multer();
+
+module.exports = upload;
