e828d77baa424bb776105f83bb57d7d36602a299
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-12-12 17:15:21 +0700

    fix setup cors

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const router = require('./app/router');
-// const { MORGAN_FORMAT } = require('../config/application');
 
 const app = express();
 
-// app.use(morgan(MORGAN_FORMAT));
+app.use(morgan('common'));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(cors());
-
 
 module.exports = app;
