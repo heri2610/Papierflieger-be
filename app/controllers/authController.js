404bd5c5afaeb426cc789767491306d41c2bbb31
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-12-27 02:00:49 +0700

    mengubah tampilan verifikasi email

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const dotenv = require('dotenv');
 const { v4: uuidv4, } = require('uuid');
 const jwt = require('jsonwebtoken');
@@ -105,7 +106,189 @@ const register = async (req, res) => {
       EMAIL: email,
       subject: 'Email Verification',
       text: 'hello word',
-      html: `${process.env.URLSENDEMAIL}?token=${token}`,
+      html: `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Email Confirmation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+        @media screen {
+          @font-face {
+            font-family: 'Source Sans Pro';
+            font-style: normal;
+            font-weight: 400;
+            src: url(https://fonts.googleapis.com/css2?family=Rubik&display=swap);
+          }
+          @font-face {
+            font-family: 'Source Sans Pro';
+            font-style: normal;
+            font-weight: 700;
+            src: url(https://fonts.googleapis.com/css2?family=Rubik&display=swap);
+          }
+        }
+        body,
+        table,
+        td,
+        a {
+          -ms-text-size-adjust: 100%;
+          -webkit-text-size-adjust: 100%;
+        }
+        table,
+        td {
+          mso-table-rspace: 0pt;
+          mso-table-lspace: 0pt;
+        }
+        img {
+          -ms-interpolation-mode: bicubic;
+        }
+        a[x-apple-data-detectors] {
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          color: inherit !important;
+          text-decoration: none !important;
+        }
+        div[style*="margin: 16px 0;"] {
+          margin: 0 !important;
+        }
+        body {
+          width: 100% !important;
+          height: 100% !important;
+          padding: 0 !important;
+          margin: 0 !important;
+        }
+        table {
+          border-collapse: collapse !important;
+        }
+        a {
+          color: #1a82e2;
+        }
+        img {
+          height: auto;
+          line-height: 100%;
+          text-decoration: none;
+          border: 0;
+          outline: none;
+        }
+        </style>
+      
+      </head>
+      <body style="background-color: #e9ecef;"><!-- start body -->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      
+          <!-- start logo -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                  <td align="center" valign="top" style="padding: 36px 24px;">
+                    <a href="https://papierflieger.netlify.app/" target="_blank" style="display: inline-block;">
+                      <img src="https://ik.imagekit.io/lscxjpnrv/logo_papierflieger.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end logo -->
+      
+          <!-- start hero -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                  <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+                    <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end hero -->
+      
+          <!-- start copy block -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+      
+                <!-- start copy -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                    <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with <a href="https://papierflieger.netlify.app">Papierflieger</a>, you can safely delete this email.</p>
+                  </td>
+                </tr>
+                <!-- end copy -->
+      
+                <!-- start button -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                      <tr>
+                        <td align="center" bgcolor="#ffffff" style="padding: 12px;">
+                          <table border="0" cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
+                                <a href="${process.env.VERIF_EMAIL}?token=${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verification Account</a>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <!-- end button -->
+      
+                <!-- start copy -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
+                    <p style="margin: 0;">regards,<br> Papierflieger</p>
+                  </td>
+                </tr>
+                <!-- end copy -->
+      
+              </table>
+            </td>
+          </tr>
+          <!-- end copy block -->
+      
+          <!-- start footer -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+      
+                <!-- start permission -->
+                <tr>
+                  <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+                    <p style="margin: 0;">You received this email because we received a request for registration for your account. If you didn't request registration you can safely delete this email.</p>
+                  </td>
+                </tr>
+                <!-- end permission -->
+      
+                <!-- start unsubscribe -->
+                <tr>
+                  <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+                    <p style="margin: 0;">© Papierflieger</p>
+                  </td>
+                </tr>
+                <!-- end unsubscribe -->
+      
+              </table>
+            </td>
+          </tr>
+          <!-- end footer -->
+      
+        </table>
+        <!-- end body -->
+      
+      </body>
+      </html>
+      `,
     };
     sendMail(data);
 
@@ -219,8 +402,7 @@ const updateProfile = async (req, res) => {
         country,
         province,
         regency,
-      },
-      {
+      }, {
         where: {
           id,
         },
