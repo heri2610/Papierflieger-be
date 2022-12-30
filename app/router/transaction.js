178d2b93caa7fa9df4ba031b5515b8b3b6938aba
heri2610 <suhaeriheri45@gmail.com>
2022-12-30 20:03:14 +0700

    menambhakan relasi transaksi dengan user di method transaksi

--- app/router/transaction.js
+++ app/router/transaction.js
@@ -3,20 +3,16 @@ const auth = require('../../midleware/auth');
 const isAdmin = require('../../midleware/isAdmin');
 const {
   addTransaction,
+  getTransactionByUser,
   getTransactionByToken,
   updateTransaction,
   deleteTransaction,
   getTransaction,
 } = require('../controllers/transactionController');
 
-<<<<<<< HEAD
 router.get('/transactions-token/:tokenTransaksi', auth, getTransactionByUser);
 router.get('/transactions',  getTransaction);
 router.get('/transactions/:id', auth, getTransactionByToken);
-=======
-router.get('/transactions', auth, isAdmin, getTransaction);
-router.get('/transactions/:tokenTransaksi', auth, getTransactionByToken);
->>>>>>> 3cc96788a4b4b4b9efedc7683f8bfc17db994ae5
 router.post('/transactions', auth, addTransaction);
 router.put('/transactions', auth, updateTransaction);
 router.delete('/transactions/:id', auth, isAdmin, deleteTransaction);
