e689f7629dd47fbd9983c2c9318b3d2ccf4cc927
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-12-31 15:26:03 +0700

    feat: send email ketika transaksi berhasil

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -1,6 +1,8 @@
-const { Transaction, Order, Ticket, Payment,Users,notification, } = require('../models');
+/* eslint-disable max-len */
+const { Transaction, Order, Ticket, Payment, Users, notification, } = require('../models');
 const { addHistory, } = require('./historyController');
-const { addPayment, } = require('./paymentController');
+const { addPayment, getPaymentByNoRek, } = require('./paymentController');
+const sendMail = require('../../lib/nodemailer');
 
 const getTransactionByUser = async (req, res) => {
   try {
@@ -33,8 +35,7 @@ const getTransaction = async (req, res) => {
   try {
     const transaksi = await Transaction.findAll(
       {
-        include: [{ model: Payment, },{ model: Users, },
-        ],
+        include: [{ model: Payment, }, { model: Users, },],
       }
     );
     res.status(200).json({
@@ -101,21 +102,250 @@ const updateTransaction = async (req, res) => {
       tokenTransaction,
     } = req.body;
     const payman = await addPayment(bankName, accountName, accountNumber);
+    const existPayment = await getPaymentByNoRek(accountNumber);
     await Transaction.update({
       status: true,
-      paymentId: payman.id,
+      paymentId: payman.id || existPayment.id,
     }, {
       where: {
         tokenTransaction,
       },
     });
+
+    const transaksi = await Transaction.findOne({
+      where: { tokenTransaction, },
+    });
+
     const data = {
       name: 'Booking Tiket Berhasil',
-      // eslint-disable-next-line max-len
-      message: `Pembayaran atas ${accountName} telah berhasil, terimakasih telah menggunkaan laynan kami untuk perjalanan anda`,
+      message: `Pembayaran atas ${accountName} telah berhasil, terimakasih telah menggunkaan layanan kami untuk perjalanan anda`,
       userId,
     };
-    await notification(data);
+    await notification.create(data);
+
+    const userEmail = req.user.email;
+    const namaPenerima = req.user.fullName;
+    const kodeTransaksi = tokenTransaction.split('-')[0].toUpperCase();
+    const formatter = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', });
+    const tglBayar = new Date();
+
+    const email = {
+      EMAIL: userEmail,
+      subject: `Bukti Pembayaran Transaksi PT. Papierflieger (${kodeTransaksi})`,
+      text: 'hello word',
+      html:
+        `<!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Email Confirmation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+          @media screen {
+            @font-face {
+              font-family: 'Source Sans Pro';
+              font-style: normal;
+              font-weight: 400;
+              src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+            }
+      
+            @font-face {
+              font-family: 'Source Sans Pro';
+              font-style: normal;
+              font-weight: 700;
+              src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+            }
+          }
+      
+          body,
+          table,
+          td,
+          a {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+          }
+      
+          img {
+            -ms-interpolation-mode: bicubic;
+          }
+      
+          a[x-apple-data-detectors] {
+            font-family: inherit !important;
+            font-size: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            color: inherit !important;
+            text-decoration: none !important;
+          }
+      
+          div[style*="margin: 16px 0;"] {
+            margin: 0 !important;
+          }
+      
+          body {
+            width: 100% !important;
+            height: 100% !important;
+            padding: 0 !important;
+            margin: 0 !important;
+          }
+      
+          table {
+            border-collapse: collapse !important;
+          }
+      
+          a {
+            color: #1a82e2;
+          }
+      
+          img {
+            height: auto;
+            line-height: 100%;
+            text-decoration: none;
+            border: 0;
+            outline: none;
+          }
+      
+          .grid-container {
+            display: grid;
+            grid-template-columns: auto auto auto;
+            background-color: #ebecf0;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+            border: 1px solid #777;
+            border-radius: 10px;
+            margin: 0 24px 20px;
+          }
+      
+          .grid-item1 {
+            font-size: 12px;
+            font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+            width: 49%;
+            font-weight: 600;
+          }
+      
+          .grid-item2 {
+            font-size: 12px;
+            font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+            width: 50%;
+          }
+        </style>
+      </head>
+      
+      <body style="background-color: #ffffff;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+          <!-- start logo -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                  <td align="center" valign="top" style="padding: 36px 24px;">
+                    <a href="https://papierflieger.netlify.app/" target="_blank" style="display: inline-block;">
+                      <img src="https://ik.imagekit.io/lscxjpnrv/logo_papierflieger.png" alt="Papierflieger" border="0"
+                        width="64" style="display: block; width: 64px; max-width: 64px; min-width: 64px;">
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end logo -->
+      
+          <!-- start hero -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                  <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; 
+                    border-top: 3px solid #d4dadf;">
+                    <h1 style="margin: 0; font-size: 20px; font-weight: 400; letter-spacing: -1px; line-height: 36x;">Halo,
+                      ${namaPenerima}!</h1>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end hero -->
+      
+          <!-- start copy block -->
+          <tr>
+            <td align="center" bgcolor="#e9ecef">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px; margin-bottom: 32px;">
+                <!-- start copy -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; 
+                    font-size: 16px; line-height: 24px;">
+                    <p style="margin: 0;"><strong>Pemesanan tiket pesawat kamu berhasil dibayar</strong></p>
+                    <p>Berikut detail pemesanan tiket kamu :</p>
+                  </td>
+                </tr>
+                <!-- end copy -->
+      
+                <!-- start button -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff">
+                    <div class="grid-container">
+                      <div class="grid-item1">
+                        <p>Total Bayar</p>
+                        <p>Pembayaran Melalui</p>
+                        <p>Pemilik Rekening</p>
+                        <p>Kode Transaksi</p>
+                        <p>Waktu Pembayaran</p>
+                      </div>
+                      <div class="grid-item2" align="right">
+                        <p>${formatter.format(transaksi.totalPrice)}</p>
+                        <p>${bankName}</p>
+                        <p>${accountName}</p>
+                        <p>${kodeTransaksi}</p>
+                        <p>${tglBayar.toLocaleString('id-ID', { timeZone: 'Asia/Jakarta', })}</p>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <!-- end button -->
+      
+                <!-- start copy -->
+                <tr>
+                  <td align="left" bgcolor="#ffffff"
+                    style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 12px; line-height: 24px; border-bottom: 3px solid #d4dadf">
+                    <p style="margin: 0;">Tidak Termasuk PPN.<br />
+                      PPN dibebaskan berdasarkan pasal 16B Undang Undang Harmonisasi Peraturan Perpajakan
+                    </p>
+                  </td>
+                </tr>
+                <!-- end copy -->
+              </table>
+            </td>
+          </tr>
+          <!-- end copy block -->
+      
+          <!-- start footer -->
+          <tr>
+            <td align="center" bgcolor="#0060c2" style="padding: 24px;">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                  <td align="center" bgcolor="#0060c2" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; 
+                    font-size: 14px; line-height: 20px; color: #fff;">
+                    <p style="margin: 0;">E-mail ini dibuat otomatis, mohon untuk tidak membalas. Jika ada pertanyaan atau
+                      membutuhkan bantuan silahkan hubungi contact center.</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td align="center" bgcolor="#0060c2" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; 
+                    font-size: 14px; line-height: 20px; color: #fff;">
+                    <p style="margin: 0;">© PT Papierflieger</p>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>`,
+    };
+    sendMail(email);
+
     res.status(200).json({
       message: 'Selamat pembayaran berhasil dilakukan!',
     });
