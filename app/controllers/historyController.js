62bf22e221eb1f1f44a05fd44c50b06c877ac879
heri2610 <suhaeriheri45@gmail.com>
2022-12-31 22:17:14 +0700

    gethistory ubah async awaut

--- app/controllers/historyController.js
+++ app/controllers/historyController.js
@@ -1,29 +1,27 @@
+/* eslint-disable max-len */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 const { History, Transaction, Order,Ticket, } = require('../models');
 
 const getHistory = async (req, res) => {
   try {
     const userId = req.user.id;
-    const transaction = await Transaction.findAll(
-      {
-        where: { userId, },
-      }
-    );
+    const transaction = await Transaction.findAll({ where: { userId, }, });
     const order = [];
-    // eslint-disable-next-line max-len, no-return-await
-    await transaction.map((item)=>item.orderId.map(async(id)=>order.push(await Order.findOne({where:{id,},}))));
-    setTimeout(() => {
-      const ticket = [];
-      // eslint-disable-next-line max-len, no-return-await
-      order.map((item)=>item.ticketId.map(async(id)=>ticket.push(await Ticket.findOne({where:{id,},}))));
-      setTimeout(() => {
-        res.status(200).json({
-          transaction,
-          order,
-          ticket,
-        });
-      }, 1000);
-    }, 1100);
- 
+    for (const trans of transaction) {
+      const transOrders = await Order.findAll({ where: { id: trans.orderId, }, });
+      order.push(...transOrders);
+    }
+    const ticket = [];
+    for (const ord of order) {
+      const ordTickets = await Ticket.findAll({ where: { id: ord.ticketId, }, });
+      ticket.push(...ordTickets);
+    }
+    res.status(200).json({
+      transaction,
+      order,
+      ticket,
+    });
   } catch (error) {
     res.status(error.statusCode || 500).json({
       message: error.message,
